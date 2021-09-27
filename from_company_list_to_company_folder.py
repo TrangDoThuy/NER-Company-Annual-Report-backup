3508f34f3b0416a12df9e9809c2c8eabd42dca0a
Emma Thuy Trang DO <30380242+TrangDoThuy@users.noreply.github.com>
2021-09-27 12:37:13 +0800

    Update dynamic agent for web scrapping

--- from_company_list_to_company_folder.py
+++ from_company_list_to_company_folder.py
@@ -1,5 +1,5 @@
 #%%
-import requests
+import requests,random
 import pandas as pd
 from bs4 import BeautifulSoup
 import time
@@ -12,6 +12,30 @@ f_json = open('Data/meta_data.json')
 json_object =json.load(f_json)
 f_json.close()
 endpoint = r"https://www.sec.gov/cgi-bin/browse-edgar"
+#%%
+user_agent_list = [
+    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/22.0.1207.1 Safari/537.1",
+    "Mozilla/5.0 (X11; CrOS i686 2268.111.0) AppleWebKit/536.11 (KHTML, like Gecko) Chrome/20.0.1132.57 Safari/536.11",
+    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.6 (KHTML, like Gecko) Chrome/20.0.1092.0 Safari/536.6",
+    "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.6 (KHTML, like Gecko) Chrome/20.0.1090.0 Safari/536.6",
+    "Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/19.77.34.5 Safari/537.1",
+    "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/536.5 (KHTML, like Gecko) Chrome/19.0.1084.9 Safari/536.5",
+    "Mozilla/5.0 (Windows NT 6.0) AppleWebKit/536.5 (KHTML, like Gecko) Chrome/19.0.1084.36 Safari/536.5",
+    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1063.0 Safari/536.3",
+    "Mozilla/5.0 (Windows NT 5.1) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1063.0 Safari/536.3",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_0) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1063.0 Safari/536.3",
+    "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1062.0 Safari/536.3",
+    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1062.0 Safari/536.3",
+    "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.1 Safari/536.3",
+    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.1 Safari/536.3",
+    "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.1 Safari/536.3",
+    "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.0 Safari/536.3",
+    "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/535.24 (KHTML, like Gecko) Chrome/19.0.1055.1 Safari/535.24",
+    "Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/535.24 (KHTML, like Gecko) Chrome/19.0.1055.1 Safari/535.24"
+]
+no_agent = len(user_agent_list)
+
+#%%
 headers = { 'User-Agent': 'Mozilla/5.0', }
 #json_object["companies"]=[]
 exchange_list=[]
@@ -20,6 +44,7 @@ parent_dir = "Data/"
 df_sub = df[497:]
 for index, row in df_sub.iterrows():
     print(index)
+
     company ={}
     company["CIK"] = row["CIK"]
     company["ticker"] = row["Ticker"]
@@ -44,16 +69,22 @@ for index, row in df_sub.iterrows():
               'CIK':company_cik,
               'type':'10-k',
               'output':'atom'}
+    
+    agent = user_agent_list[index % no_agent]
+    headers = {'User-Agent': agent}
+    
     response_10_K_list = requests.get(url = endpoint, params = param_dict_2,headers = headers)
-    time.sleep(0.1)
+    #time.sleep(0.1)
     soup_10_K_list = BeautifulSoup(response_10_K_list.content, ('html.parser'))
     link_list = []
     for each_10_K in soup_10_K_list.find_all('entry'):
         link_10_K = each_10_K.find_all('link')[0]['href']
         link_list.append(link_10_K)
-
+         
+        agent = random.choice(user_agent_list)
+        headers = {'User-Agent': agent}
         response = requests.get(url = link_10_K,headers = headers)
-        time.sleep(0.1)
+        #time.sleep(0.1)
         soup = BeautifulSoup(response.content,('html.parser'))
         json_object_file ={}
         report_period_title = soup.find(text="Period of Report")
@@ -78,8 +109,10 @@ for index, row in df_sub.iterrows():
         
         # create 10-K file locally
         local_path = company_path+"/annual_report_"+filing_date+"_"+name_file
+        agent = random.choice(user_agent_list)
+        headers = {'User-Agent': agent}
         r = requests.get(link_10K, stream=True, headers = headers)
-        time.sleep(0.1)
+        #time.sleep(0.1)
         json_object_file["file_directory"] = local_path
         with open(local_path, 'wb') as f:
             for chunk in r.iter_content(chunk_size=10240):
